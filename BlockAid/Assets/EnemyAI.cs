dd1f180ac0c734cdd680adbc04aad78075ce67fc
Temmie <24ab@student.nbcs.nsw.edu.au>
2023-05-10 18:35:40 +1000

    Moved all assets to new pipeline
    Created a new unity project with the Universal Rendering Pipeline and moved all scripts over to the new project as well as the game objects. Old pipeline file is kept in the repo but is marked as old

---
+++ BlockAid/Assets/EnemyAI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+    private float distance;
+
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        transform.position = transform.position + (Vector3)direction * speed * Time.deltaTime;
+    }
+
+}
