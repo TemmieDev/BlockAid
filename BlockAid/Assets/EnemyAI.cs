6e3012912bac2ca0f46778bf1d9f6010f9188b3b
PhantomG901 <24wb@student.nbcs.nsw.edu.au>
2023-05-11 19:24:35 +0000

    Attempted to make the AI feel more lively (failed attempt). Accidently made the enemy run away from the player instead

--- BlockAid/Assets/EnemyAI.cs
+++ BlockAid/Assets/EnemyAI.cs
@@ -4,8 +4,14 @@ public class EnemyAI : MonoBehaviour
 {
     public Transform player;
     public float speed;
+    public float contactRadius = 1.5f; // Distance at which the enemy will make contact with the player
+    public float avoidanceRadius = 2f; // Distance at which the enemy will avoid the player
+    public float avoidanceTime = 2f; // Time the enemy will avoid the player before returning to normal behavior
 
     private Rigidbody2D rb;
+
+    private bool isAvoidingPlayer = false;
+    private float avoidanceTimer = 0f;
 
     void Start()
     {
@@ -14,7 +20,45 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
-        Vector2 direction = (player.position - transform.position).normalized;
-        rb.velocity = direction * speed;
+        // Check if the player is within contact radius
+        if (Vector2.Distance(transform.position, player.position) <= contactRadius)
+        {
+            // Move towards the player
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.velocity = direction * speed;
+        }
+        else if (isAvoidingPlayer) 
+        {
+            // Avoid the player for a set amount of time before returning to normal behavior
+            avoidanceTimer -= Time.deltaTime;
+            if (avoidanceTimer <= 0f) 
+            {
+                isAvoidingPlayer = false;
+            }
+            else 
+            {
+                // Move away from the player
+                Vector2 direction = (transform.position - player.position).normalized;
+                rb.velocity = direction * speed;
+            }
+        } 
+        else 
+        {
+            // Move towards the player, but occasionally move away
+            if (Random.value < 0.95f) // 95% chance of moving towards player
+            {
+                Vector2 direction = (player.position - transform.position).normalized;
+                rb.velocity = direction * speed;
+            } 
+            else // 5% chance of moving away from player
+            {
+                Vector2 direction = (transform.position - player.position).normalized;
+                rb.velocity = direction * speed;
+
+                // Start avoiding the player for a set amount of time
+                isAvoidingPlayer = true;
+                avoidanceTimer = avoidanceTime;
+            }
+        }
     }
 }
