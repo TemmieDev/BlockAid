03768d65b0785083e0b2cc61d692c84147da7390
Temmie <24ab@student.nbcs.nsw.edu.au>
2023-07-19 13:13:17 +1000

    Sorted all Textures and Renamed all Unnamed

--- Blockaid (Godot)/Player.gd
+++ Blockaid (Godot)/Player.gd
@@ -16,6 +16,7 @@ func _physics_process(delta):
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	)
 	direction = direction.normalized()
+
 	
 	velocity = velocity.lerp(direction * player_speed, 0.1)
 	velocity *= 1.0 - (friction * delta)
