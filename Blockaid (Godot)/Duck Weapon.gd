bf5e6cbbfe813d96d14ce2c45a827207339e8d58
Temmie <24ab@student.nbcs.nsw.edu.au>
2023-08-03 13:23:23 +1000

    started on my redo of the gun function

--- Blockaid (Godot)/Duck Weapon.gd
+++ Blockaid (Godot)/Duck Weapon.gd
@@ -1,20 +1,12 @@
 extends Node2D
 
+@export var bullet = preload("res://bullet.tscn")
+@onready var end_of_weapon = $EndofWeapon
 
-signal player_fired_bullet(bullet)
-
-var bullet = preload("res://bullet.tscn")
-
-@onready var end_of_gun = $EndofWeapon
-
-func _unhandled_input(event):
-	if event.is_action_released('shoot'):
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot"):
 		shoot()
-		
+
 func shoot():
 	var bullet_instance = bullet.instantiate()
-	bullet_instance.global_position = end_of_gun.global_position
-	var target = get_global_mouse_position()
-	var direction_to_mouse = bullet_instance.global_position.direction_to(target)
-	bullet_instance.set_direction(direction_to_mouse)
-	emit_signal("player_fired_bullet", bullet_instance)
+	add_child(bullet_instance)
