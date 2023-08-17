a5dba69dcf318541adbf6d41e02ada469ca497d7
Temmie <24ab@student.nbcs.nsw.edu.au>
2023-08-17 12:43:57 +1000

    testing AI assisted fix

--- Blockaid (Godot)/AI.gd
+++ Blockaid (Godot)/AI.gd
@@ -1,25 +1,22 @@
 extends Node2D
 
-
 signal state_changed(new_state)
 
 enum State {
-	PATROL,
-	ENGAGE,
+PATROL,
+ENGAGE
 }
 
-@onready var player_detection_zone = $PlayerDectectionZone
+@onready var player_detection_zone = $PlayerDetectionZone
 
-var current_state = int(State.PATROL set_state)
+var current_state = State.PATROL
 
-func set_state(new_state: int):
+func set_state(new_state: State):
 	if new_state == current_state:
 		return
-	
-	current_state = new_state 
-	emmit_signal("state_changed", current_state)
+		current_state = new_state
+	emit_signal("state_changed", current_state)
 
 
 func _on_PlayerDetectionZone_body_entered(body: Node) -> void:
 	pass
-
